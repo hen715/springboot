4f06ed2734963149d53ff3203e05387b464c2cdf
hen715 <hen715@naver.com>
2023-07-19 02:59:52 +0900

    주석 추가

--- src/main/java/spring_study/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/spring_study/springboot/web/dto/HelloResponseDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor//생성자 주입
 public class HelloResponseDto {
 
     private final String name;
