dfc54a70a9894bcf6e9b13c9b80529af16633955
hen715 <hen715@naver.com>
2023-07-19 03:00:36 +0900

    jpa 테스트 코드

---
+++ src/test/java/spring_study/springboot/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package spring_study.springboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring_study.springboot.domain.Posts;
+import spring_study.springboot.domain.PostsRepository;
+
+import java.util.List;
+
+@SpringBootTest
+//@AutoConfigureMockMvc
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hen715@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
