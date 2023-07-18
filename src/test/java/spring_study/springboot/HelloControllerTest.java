26e6697b4aa34bfac10d509169eaa592eabc1944
hen715 <hen715@naver.com>
2023-07-19 00:08:59 +0900

    테스트코드에 dto테스트 추가

--- src/test/java/spring_study/springboot/HelloControllerTest.java
+++ src/test/java/spring_study/springboot/HelloControllerTest.java
@@ -7,10 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,5 +25,14 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.name").value(name)).andExpect(jsonPath("$.amount").value(amount));
     }
 }
