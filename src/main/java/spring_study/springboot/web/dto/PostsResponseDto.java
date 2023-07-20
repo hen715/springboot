34e1dc0d63f1cdd0780a819febcfa5e438ef329c
hen715 <hen715@naver.com>
2023-07-20 23:57:18 +0900

    조회 시 객체를 가져오는 Dto

---
+++ src/main/java/spring_study/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package spring_study.springboot.web.dto;
+
+import lombok.Getter;
+import spring_study.springboot.domain.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
