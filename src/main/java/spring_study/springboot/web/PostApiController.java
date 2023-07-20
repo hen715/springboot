34751a917e0858de5e1eb86549e5c9b556944817
hen715 <hen715@naver.com>
2023-07-20 23:54:00 +0900

    업데이트, 아이디로 조회기능 추가

---
+++ src/main/java/spring_study/springboot/web/PostApiController.java
@@ -0,0 +1,32 @@
+package spring_study.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import spring_study.springboot.domain.PostsRepository;
+import spring_study.springboot.service.posts.PostsService;
+import spring_study.springboot.web.dto.PostsResponseDto;
+import spring_study.springboot.web.dto.PostsSaveRequestDto;
+import spring_study.springboot.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
