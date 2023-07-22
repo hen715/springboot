ceb483bcf1ef3bc3a662699c5e7d0219a06615a4
hen715 <hen715@naver.com>
2023-07-22 23:20:39 +0900

    컨트롤러에 게시글 불러오는 기능 추가

--- src/main/java/spring_study/springboot/web/IndexController.java
+++ src/main/java/spring_study/springboot/web/IndexController.java
@@ -1,12 +1,18 @@
 package spring_study.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import spring_study.springboot.service.posts.PostsService;
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
