3391782d862c0f3ca2d56027af2ee7d36cf419ea
hen715 <hen715@naver.com>
2023-07-24 18:40:10 +0900

    LoginUser 다루는 클래스를 다루는 클래스

---
+++ src/main/java/spring_study/springboot/config/auth/WebConfig.java
@@ -0,0 +1,20 @@
+package spring_study.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {//LoginUserArgumentResolver 다루는 클래스
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
